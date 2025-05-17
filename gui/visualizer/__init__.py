1acbd24e3e1c10a4317acb3915752cd3b8b45b3c
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-05-17 00:02:51 -0500

    Update __init__.py

--- gui/visualizer/__init__.py
+++ gui/visualizer/__init__.py
@@ -1,1 +1,5 @@
+# Gui/visualizer/__init__.py
+
+# This file marks the 'Gui/visualizer' directory as a Python package.
+# You can add initialization code or import commonly used visualization modules here.
 
