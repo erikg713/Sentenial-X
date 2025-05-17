b408347da37f22f4942d22e839772abb457288b2
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-05-17 00:05:40 -0500

    Update __init__.py

--- core/sandbox_vm/__init__.py
+++ core/sandbox_vm/__init__.py
@@ -1,1 +1,5 @@
+# Core/sandbox_vm/__init__.py
+
+# This file marks the 'Core/sandbox_vm' directory as a Python package.
+# You can add initialization code or define modules related to your sandbox VM integration here.
 
