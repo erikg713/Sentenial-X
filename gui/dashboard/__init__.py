ab54acbac76d897d2c4f6e571c157e0f1d969db7
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-05-17 00:03:27 -0500

    Update __init__.py

--- gui/dashboard/__init__.py
+++ gui/dashboard/__init__.py
@@ -1,1 +1,5 @@
+# Gui/dashboard/__init__.py
+
+# This file marks the 'Gui/dashboard' directory as a Python package.
+# You can add initialization code or import commonly used dashboard components here.
 
