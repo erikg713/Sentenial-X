24b022366b6367df4753e18c2859c2b867ba0b21
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-05-17 00:00:06 -0500

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,1 +1,5 @@
+# Plugins/__init__.py
+
+# This file marks the 'Plugins' directory as a Python package.
+# You can add initialization code or define which modules are exposed when the package is imported.
 
