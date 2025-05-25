a6bd9af256df30802da86541673542830c66a6ec
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-05-25 14:43:43 +0000

    Create __init__.py

---
+++ agents/__init__.py
@@ -0,0 +1,29 @@
+"""
+sentenial_core.interfaces
+
+Defines agents for SentenialXAi system.
+"""
+
+from abc import ABC, abstractmethod
+
+__all__ = [
+    # List public interface classes here as you add them, e.g.:
+    # "DataProvider",
+    # "EventListener",
+]
+
+# Example interface template for future use.
+class InterfaceTemplate(ABC):
+    """
+    Abstract base class template for SentenialX interfaces.
+    """
+
+    @abstractmethod
+    def process(self, *args, **kwargs):
+        """
+        Process data or events. Should be implemented by subclasses.
+        """
+        pass
+
+# Clean up namespace
+del ABC, abstractmethod
