06cf2b65d2884398a491b3a32e8605ae255611dc
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-15 15:55:11 -0500

    Update __init__.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- gui/dashboard/widgets/__init__.py
+++ gui/dashboard/widgets/__init__.py
@@ -1,1 +1,18 @@
+# gui/dashboard/widgets/__init__.py
+
+from .threat_table import ThreatTable
+from .telemetry_viewer import TelemetryViewer
+from .simulation_controls import SimulationControls
+from .report_card import ReportCard
+from .attack_graph import AttackGraph
+from .exploit_module import ExploitModuleWidget
+
+__all__ = [
+    "ThreatTable",
+    "TelemetryViewer",
+    "SimulationControls",
+    "ReportCard",
+    "AttackGraph",
+    "ExploitModuleWidget"
+]
 
