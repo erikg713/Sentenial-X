c423fe0938badddb4218a018ffe35751a2958b51
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-17 15:22:04 -0500

    Update save_utils.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- apps/ml_pipeline/save_utils.py
+++ apps/ml_pipeline/save_utils.py
@@ -1,1 +1,8 @@
+def save_model(model, vectorizer, save_path: Path, metadata: dict):
+    data = {
+        "model": model,
+        "vectorizer": vectorizer,
+        "metadata": metadata
+    }
+    joblib.dump(data, save_path)
 
