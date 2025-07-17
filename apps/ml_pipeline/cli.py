2ef66ddc354477e953d973d57e6dff0debd92f3f
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-17 15:22:36 -0500

    Update cli.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- apps/ml_pipeline/cli.py
+++ apps/ml_pipeline/cli.py
@@ -1,1 +1,8 @@
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("feedback_file", type=str)
+    parser.add_argument("--output", type=str, default="secure_db/model.pkl")
+    parser.add_argument("--dry-run", action="store_true")
+    parser.add_argument("--verbose", action="store_true")
+    return parser.parse_args()
 
