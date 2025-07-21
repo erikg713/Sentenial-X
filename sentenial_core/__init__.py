02bf0a9d416f42af52c0221230229c6ea45862f5
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-21 07:31:36 -0500

    Update __init__.py

--- sentenial_core/__init__.py
+++ sentenial_core/__init__.py
@@ -1,1 +1,11 @@
-t
+from flask import Flask
+from .config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Add your blueprints, db, etc here
+    # e.g. db.init_app(app), app.register_blueprint(...)
+
+    return app
