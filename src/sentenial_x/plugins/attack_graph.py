d97692a5e1c7cdceef8e828df2c2816bb29db979
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-28 04:48:47 -0500

    Update attack_graph.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- src/sentenial_x/plugins/attack_graph.py
+++ src/sentenial_x/plugins/attack_graph.py
@@ -1,74 +1,38 @@
-{
-  "plugins": [
-    {
-      "name": "attack-graph",
-      "version": "0.1.0",
-      "module": "sentenial_x.plugins.attack_graph",
-      "class": "AttackGraphPlugin",
-      "file": "src/sentenial_x/plugins/attack_graph.py"
-    },
-    {
-      "name": "api-gatekeeper",
-      "version": "0.1.0",
-      "module": "sentenial_x.plugins.api_gatekeeper",
-      "class": "ApiGatekeeperPlugin",
-      "file": "src/sentenial_x/plugins/api_gatekeeper.py"
-    },
-    {
-      "name": "insider-watch",
-      "version": "0.1.0",
-      "module": "sentenial_x.plugins.insider_watch",
-      "class": "InsiderWatchPlugin",
-      "file": "src/sentenial_x/plugins/insider_watch.py"
-    },
-    {
-      "name": "dlp-sentinel",
-      "version": "0.1.0",
-      "module": "sentenial_x.plugins.dlp_sentinel",
-      "class": "DLPSentinelPlugin",
-      "file": "src/sentenial_x/plugins/dlp_sentinel.py"
-    },
-    {
-      "name": "cloud-least-privilege",
-      "version": "0.1.0",
-      "module": "sentenial_x.plugins.cloud_least_privilege",
-      "class": "CloudLeastPrivilegePlugin",
-      "file": "src/sentenial_x/plugins/cloud_least_privilege.py"
-    },
-    {
-      "name": "scada-guardian",
-      "version": "0.1.0",
-      "module": "sentenial_x.plugins.scada_guardian",
-      "class": "ScadaGuardianPlugin",
-      "file": "src/sentenial_x/plugins/scada_guardian.py"
-    },
-    {
-      "name": "war-game",
-      "version": "0.1.0",
-      "module": "sentenial_x.plugins.war_game",
-      "class": "WarGamePlugin",
-      "file": "src/sentenial_x/plugins/war_game.py"
-    },
-    {
-      "name": "twin-lab",
-      "version": "0.1.0",
-      "module": "sentenial_x.plugins.twin_lab",
-      "class": "TwinLabPlugin",
-      "file": "src/sentenial_x/plugins/twin_lab.py"
-    },
-    {
-      "name": "phish-trainer",
-      "version": "0.1.0",
-      "module": "sentenial_x.plugins.phish_trainer",
-      "class": "PhishTrainerPlugin",
-      "file": "src/sentenial_x/plugins/phish_trainer.py"
-    },
-    {
-      "name": "sec-chatbot",
-      "version": "0.1.0",
-      "module": "sentenial_x.plugins.sec_chatbot",
-      "class": "SecChatbotPlugin",
-      "file": "src/sentenial_x/plugins/sec_chatbot.py"
-    }
-  ]
-}
+from sentenial_x.plugins import PluginBase
+from networkx import DiGraph, shortest_path_length
+
+class AttackGraphPlugin(PluginBase):
+    """
+    Ingest assets + topology, build attacker-path graph, emit prioritized choke points.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.graph = DiGraph()
+
+    def on_inventory_update(self, event):
+        assets = event.payload  # list of asset dicts
+        for asset in assets:
+            self.graph.add_node(asset['id'], **asset)
+
+    def on_topology_update(self, event):
+        links = event.payload  # list of (src, dst, attributes)
+        for src, dst, attrs in links:
+            self.graph.add_edge(src, dst, **attrs)
+
+    def on_scheduled_task(self, event):
+        # compute shortest paths from internet-facing nodes to critical assets
+        critical = [n for n, data in self.graph.nodes(data=True) if data.get('critical')]
+        internet_facing = [n for n, data in self.graph.nodes(data=True) if data.get('exposed')]
+        choke_scores = {}
+        for i in internet_facing:
+            for c in critical:
+                try:
+                    dist = shortest_path_length(self.graph, source=i, target=c)
+                    choke_scores[(i, c)] = dist
+                except Exception:
+                    continue
+
+        # pick top 5 shortest paths as highest risk
+        prioritized = sorted(choke_scores.items(), key=lambda x: x[1])[:5]
+        self.emit("on_attack_paths_computed", {"paths": prioritized})
