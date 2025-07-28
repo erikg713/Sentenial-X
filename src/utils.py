e86b8c1069e82802a3c12f3556314f90081d582e
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-28 04:09:30 -0500

    Update utils.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,8 @@
+import yaml
+from omegaconf import OmegaConf
+
+def load_config(path: str = "config.yml"):
+    with open(path, "r") as f:
+        data = yaml.safe_load(f)
+    return OmegaConf.create(data)
 
