79e3ac03d806cb89e81f6b1ebb4bb4e09c159672
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-28 05:33:42 -0500

    Create brainstem.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

---
+++ core/cortex/brainstem.py
@@ -0,0 +1,16 @@
+class Brainstem:
+    def __init__(self):
+        self.status = "idle"
+        self.last_signal = None
+
+    def process_signal(self, signal):
+        self.last_signal = signal
+        self.status = "processing"
+        # Simulate initial brainstem reflex
+        if signal.get("threat_level", 0) > 7:
+            return {"reflex": "isolate_system"}
+        return {"reflex": "log_and_monitor"}
+
+    def reset(self):
+        self.status = "idle"
+        self.last_signal = None
