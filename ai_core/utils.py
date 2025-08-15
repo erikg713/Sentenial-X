30841382a6efc3bf1604a8c5a6b1d06db63f14c3
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-08-15 18:29:26 -0500

    Update utils.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- ai_core/utils.py
+++ ai_core/utils.py
@@ -1,1 +1,9 @@
+# sentenial-x/ai_core/utils.py
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 
+def similarity_score(vec_a: np.ndarray, vec_b: np.ndarray) -> float:
+    """
+    Compute cosine similarity between two embeddings.
+    """
+    return float(cosine_similarity(vec_a.reshape(1, -1), vec_b.reshape(1, -1))[0][0])
