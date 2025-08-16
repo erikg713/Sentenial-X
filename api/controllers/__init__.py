fe42ebb7e9bd325e218c9f365ddbc0ef8f30f44a
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-08-16 13:01:11 -0500

    Update __init__.py

--- api/controllers/__init__.py
+++ api/controllers/__init__.py
@@ -1,1 +1,26 @@
- 
+"""
+API Controllers Package
+
+This package contains all controller modules responsible for
+handling request/response logic for the Palace of Quests backend.
+"""
+
+from .auth_controller import *
+from .player_controller import *
+from .quest_controller import *
+from .inventory_controller import *
+from .marketplace_controller import *
+from .transaction_controller import *
+from .battle_controller import *
+from .monitor_controller import *
+
+__all__ = [
+    "auth_controller",
+    "player_controller",
+    "quest_controller",
+    "inventory_controller",
+    "marketplace_controller",
+    "transaction_controller",
+    "battle_controller",
+    "monitor_controller",
+] 
