c11b04328bb14f31e52dd182a2cc6b200a0e3abe
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-08-25 21:56:42 -0500

    Update outputs.tf
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,1 +1,14 @@
+output "db_endpoint" {
+  description = "Postgres endpoint"
+  value       = aws_db_instance.sentenial_postgres.address
+}
 
+output "cache_endpoint" {
+  description = "Redis endpoint"
+  value       = aws_elasticache_cluster.sentenial_cache.configuration_endpoint
+}
+
+output "kafka_bootstrap_brokers" {
+  description = "Kafka bootstrap broker string"
+  value       = aws_msk_cluster.sentenial_kafka.bootstrap_brokers
+}
