107e421692a24ada2a591bdd4a2dd85e13030615
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-08-28 07:49:51 -0500

    Update kmeans.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- core/semantic_analyzer/models/clustering/kmeans.py
+++ core/semantic_analyzer/models/clustering/kmeans.py
@@ -1,1 +1,18 @@
+"""
+KMeans Clustering
+Semantic clustering of events or embeddings.
+"""
 
+from sklearn.cluster import KMeans
+import numpy as np
+
+class KMeansCluster:
+    def __init__(self, n_clusters=5):
+        self.n_clusters = n_clusters
+        self.model = KMeans(n_clusters=self.n_clusters)
+
+    def fit(self, X: np.ndarray):
+        self.model.fit(X)
+
+    def predict(self, X: np.ndarray):
+        return self.model.predict(X)
