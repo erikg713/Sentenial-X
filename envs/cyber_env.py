15b6539407eeb3eb4b3e57e77daa6a8c81d48d2d
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-09-08 17:50:50 -0500

    Update cyber_env.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- envs/cyber_env.py
+++ envs/cyber_env.py
@@ -1,1 +1,8 @@
+from cyberbattle.simulation import env
 
+
+def create_environment():
+    """Initialize CyberBattleSim environment."""
+    print("[+] Launching CyberBattleSim environment...")
+    environment = env.CyberBattleEnv()
+    return environment
