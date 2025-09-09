3418947603fec44432b5697b21b48569c437a78c
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-09-08 20:20:02 -0500

    Update a3c_agent.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- sentenial-x-ai/agents/a3c_agent.py
+++ sentenial-x-ai/agents/a3c_agent.py
@@ -1,1 +1,13 @@
+from stable_baselines3 import A2C
+from agents.base_agent import BaseAgent
+from stable_baselines3.common.vec_env import DummyVecEnv
 
+class A2CAgent(BaseAgent):
+    def setup(self):
+        n_envs = int(self.config.get("n_envs", 2))
+        self.vec_env = DummyVecEnv([self.env_fn for _ in range(n_envs)])
+        self.model = A2C("MlpPolicy", self.vec_env, verbose=1)
+    def execute(self):
+        self.model.learn(total_timesteps=self.config.get("timesteps_per_execute", 10000))
+    def teardown(self):
+        self.vec_env.close()
