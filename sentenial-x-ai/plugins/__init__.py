c7ed0b1756a91dc8a535028f4429b085176aec93
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-09-09 04:00:44 -0500

    Update __init__.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- sentenial-x-ai/plugins/__init__.py
+++ sentenial-x-ai/plugins/__init__.py
@@ -1,1 +1,9 @@
-# makes 'plugins' a package
+"""
+Sentenial-X AI Plugin System
+----------------------------
+
+Allows dynamic loading of external plugins (threat detectors, analyzers, tools).
+"""
+from .loader import PluginLoader
+
+__all__ = ["PluginLoader"]
