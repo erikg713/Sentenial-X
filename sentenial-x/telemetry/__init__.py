ec451067c996ee7d100a900870e90b3e2083320b
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-11-07 22:12:45 -0600

    Update __init__.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- sentenial-x/telemetry/__init__.py
+++ sentenial-x/telemetry/__init__.py
@@ -1,1 +1,9 @@
+# sentenialx/telemetry/__init__.py
+from .collector import TelemetryCollector, emit_telemetry
+from .schema import TelemetryRecord
 
+__all__ = [
+    "TelemetryCollector",
+    "emit_telemetry",
+    "TelemetryRecord",
+]
