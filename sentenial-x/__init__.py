71ddeb652a667deaa7587a3f3c5afafb52bde802
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-11-07 22:53:55 -0600

    Update __init__.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- sentenial-x/__init__.py
+++ sentenial-x/__init__.py
@@ -1,1 +1,5 @@
+from .sentenialx import DecisionEngine, emit_telemetry, app as fastapi_app
+from .sentenialx import ML
 
+__version__ = "2.0.0"
+__all__ = ["DecisionEngine", "emit_telemetry", "fastapi_app", "ML"]
