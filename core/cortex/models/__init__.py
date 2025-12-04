b08852bcc40bd0aebfcc52078e2fc5a0376cee6f
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-12-04 01:03:34 -0600

    Update __init__.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- core/cortex/models/__init__.py
+++ core/cortex/models/__init__.py
@@ -1,1 +1,19 @@
+"""
+Sentenial-X :: Cortex Models Package
+===================================
 
+Purpose:
+    Expose all model classes in a unified namespace for Cortex components:
+        - NLPModel (TF-IDF / BERT wrapper)
+        - BertClassifier (HuggingFace BERT)
+        - Other future ML/NLP models
+"""
+
+from .nlp_model import NLPModel
+from .bert_classifier import BertClassifier
+
+# Optional: default exports for easier imports
+__all__ = [
+    "NLPModel",
+    "BertClassifier"
+]
